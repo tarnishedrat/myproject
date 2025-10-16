292722381e9f1d1292b3370c8180c17be815122c
omar <omarzaatir@outlook.fr>
2025-10-16 03:47:39 -0700

    made the img appear randomly for no reason

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('this is branch ghareeb')
+
+
+let img = document.querySelector('img')
+
+setInterval(() => {
+    
+    let ran = Math.floor(Math.random()*600)
+    img.style.position ='absolute'
+    img.style.top = ran +'px'
+    img.style.left = ran +"px"
+}, 1000);
